38a57ddb7eaecb733c40460cbdf3071375f525d3
puntoenv <sebssjb@gmail.com>
2024-07-26 21:10:02 -0500

    chore: Update favicon.svg and Header.astro components

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import type { HTMLTag } from "astro/types";
+
 const getThemePreference = () => {
   if (typeof localStorage !== "undefined" && localStorage.getItem("theme")) {
     return localStorage.getItem("theme");
@@ -19,3 +21,29 @@ if (typeof localStorage !== "undefined") {
     attributeFilter: ["class"],
   });
 }
+
+window.addEventListener("scroll", function () {
+  const profilePicture = document.querySelector(
+    "#profile-picture",
+  ) as HTMLElement;
+  const { style } = profilePicture;
+  const { scrollY } = window;
+
+  if (scrollY > 70) {
+    style.width = "50px";
+    style.height = "50px";
+    style.top = "0.275%";
+    style.left = "calc(50% - 25px)";
+    style.transition = "all 0.5s ease";
+    style.transform = "translate(0%, 50%)";
+    style.position = "fixed";
+  } else {
+    style.width = "150px";
+    style.height = "150px";
+    style.top = "10%";
+    style.left = "calc(50% - 75px)";
+    style.transition = "all 0.5s ease";
+    style.transform = `translate(0, 0)`;
+    style.position = "absolute";
+  }
+});
