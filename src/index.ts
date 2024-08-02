39a0fb9f42f02068e03ae0b078d47c1fa4d89279
puntoenv <sebssjb@gmail.com>
2024-08-01 20:12:38 -0500

    chore: Update globals.css, create NavItem and HorizontalLine components, update package.json, update ExperienceIcon color, update astro.config.mjs, create SectionContainer component, delete ExperienceList component, update Badge component, and remove unused code from index.ts, SocialPill.astro, and Experience.astro

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
-const getThemePreference = () => {
-  if (typeof localStorage !== "undefined" && localStorage.getItem("theme")) {
-    return localStorage.getItem("theme");
-  }
-  return window.matchMedia("(prefers-color-scheme: dark)").matches
-    ? "dark"
-    : "light";
-};
-const isDark = getThemePreference() === "dark";
-document.documentElement.classList[isDark ? "add" : "remove"]("dark");
-
 // window.addEventListener("scroll", function () {
 //   const profilePicture = document?.querySelector(
 //     "#profile-picture",
