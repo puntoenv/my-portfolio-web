735253490a9d28ae5e84f159dc716d447f625f08
puntoenv <sebssjb@gmail.com>
2024-08-03 18:18:37 -0500

    chore: Update icons, colors, and styles in various components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-blue": "#9C7BF4",
+        "dark-blue": "#422199",
+      },
+    },
   },
   plugins: [],
 };
