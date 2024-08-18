0e67d20198c1c1b74fb0ff216c611df69af8c19d
puntoenv <sebssjb@gmail.com>
2024-08-17 19:41:23 -0500

    ADD: Redirect and SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: "server",
+  adapter: node({
+    mode: 'middleware',
+  }),
+  redirects: {
+    "/[...slug]": "/"
+  }
 });
