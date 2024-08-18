182970013fe2ad9f7da4dbfb5bf40bc0e39fa3ab
puntoenv <sebssjb@gmail.com>
2024-08-17 19:51:34 -0500

    Revert "ADD: Redirect and SSR"
    This reverts commit 0e67d20198c1c1b74fb0ff216c611df69af8c19d.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
-  adapter: node({
-    mode: 'middleware',
-  }),
-  redirects: {
-    "/[...slug]": "/"
-  }
 });
